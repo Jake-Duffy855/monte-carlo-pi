fa7eacb8e7a7543a0b903b9f4b041e22d95b619d
Jake Duffy <duffyjemmett@gmail.com>
2019-10-28 17:56:02 -0400

    added plots of randoms dots

--- mc-pi.py
+++ mc-pi.py
@@ -1,4 +1,40 @@
 import random
+import matplotlib.pyplot as plt
+import numpy
+
+def approx_pi_with_plot(n, seed = 0):
+        random.seed(seed)
+        inside = 0
+        total = 0
+        inside_x = []
+        inside_y = []
+        outside_x = []
+        outside_y = []
+        
+        for i in range(n):
+                x = random.random()
+                y = random.random()
+                dist = dist_to_origin(x, y)
+
+                if dist <= 1:
+                        inside += 1
+                        inside_x.append(x)
+                        inside_y.append(y)
+                else:
+                        outside_x.append(x)
+                        outside_y.append(y)
+                total += 1
+
+
+        pi = 4 * inside / total
+
+        plt.plot(inside_x, inside_y, 'bo', outside_x, outside_y, 'go')
+        plt.axis([-0.03, 1.03, -0.03, 1.03])
+        plt.xlabel("Pi ~ {}, Total Dots: {}, Dots Inside {}".format(pi, inside, total))
+        plt.show()
+        
+        return pi
+
 
 def approx_pi(n, seed = 0):
         random.seed(seed)
@@ -6,20 +42,22 @@ def approx_pi(n, seed = 0):
         total = 0
         
         for i in range(n):
-                point = [random.random(), random.random()]
-                dist = dist_to_origin(point)
-
-                if dist <= 1:
+                if dist_to_origin(random.random(), random.random()) <= 1:
                         inside += 1
                 total += 1
 
-        return  4 * inside / total
+        return 4 * inside / total
 
 
+#returns the distance from point (x,y) to (0,0)
+def dist_to_origin(x, y):
+        return (x ** 2 + y ** 2) ** 0.5
 
-#returns the distance from point (list of two floats) to (0,0)
-def dist_to_origin(p):
-        return (p[0] ** 2 + p[1] ** 2) ** 0.5
 
+n = int(input("N: "))
+seed = float(input("Seed: "))
 
-print(approx_pi(int(input("N: ")), float(input("Seed: "))))
+if n <= 100000:
+        print(approx_pi_with_plot(n, seed))
+else:
+        print(approx_pi(n, seed))
